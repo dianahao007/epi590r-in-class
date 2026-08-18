335454c946617bc34e347c6c63f3d908ca002ff2
Zixuan Hao <dianahao007@hotmail.com>
2026-08-18 09:21:28 -0400

    updated code in regression r code and table

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -3,7 +3,11 @@ nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "income", "res_1980", "res_2002", "age_bir")
 
 library(tidyverse)
-setwd("~/Documents/Teaching/Emory/epi590r-inclass/data/raw/")
+#setwd("/Users/diana/Desktop/epi590r/epi590r-in-class/data/raw")
+
+library(here)
+here::here("data", "raw", "data.csv")
+
 nlsy <- read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
